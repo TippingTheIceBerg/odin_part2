660dc471fc331ffdf94a008dc29ec48940ec0f14
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-03-23 13:46:09 -0500

    add route parameters

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-// express will handle most of this.
-let http = require("http");
-let server = http.createServer((req,res)=>{
-    // mime types or media types, the codes.
-if(req.url === "/"){
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("<h1>Home Page </h1>")
-    res.end()
-}
-else if(req.url === "/about"){
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("<h1>About Page</h1>")
-    res.end()
-}
-// 404 
-else{
-    res.writeHead(404,{"content-type":"text/html"})
-    res.write("<h1>Page not fun...</h1>")
-    res.end()
-}
+const express = require("express");
+let app = express();
+const {products} = require("./data")
 
-
+app.get("/",(req,res)=>{
+    res.send('<h1>Home page </h1> <a href="/api/products">products </a>')
 })
-server.listen(5001)
+app.get("/api/products",(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product
+        return  {id,name,image}
+    })
+res.json(newProducts)
+})
+
+app.get("/api/products/:productID",(req,res)=>{
+    const {productID} = req.params;
+    const singleProduct = products.find((product)=>
+        product.id === Number(productID)
+    )
+    if(!singleProduct){
+        return res.status(404).send("Product Does Not Exist")
+    }
+    res.json(singleProduct)
+})
+
+app.get("/api/v1/query",(req,res)=>{
+    console.log(req.query);
+    res.send("Hello world")
+})
+
+app.listen(5001, ()=>{
+    console.log("Server is listening to port 5001");
+})
